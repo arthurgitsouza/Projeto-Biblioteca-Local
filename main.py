2172becb9569284bf426bd8d9418e0377f006c26
Joao Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-10-25 18:31:40 -0300

    Bug na função listar_disponiveis

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 import pandas as pd
+import os
 from tabulate import  tabulate
 from time import sleep
+import csv
 
 livros = {}
 usuarios = {}
 livros_emprestados = {}
-
+# Depois usar um contador para contar a quantidade de usuarios e livros para usar como index na tabela!
 class livro:
     def adicionar_livro(id, nome, autor):
-       livro = {'Id do Livro: ': {id}, 'Nome do Livro:': {nome}, 'Autor do Livro': {autor}} 
+       livro = {'Id do Livro:': id, 'Nome do Livro:': nome, 'Autor do Livro:': autor} 
        if nome not in livros: 
             livros[nome] = livro
             print('\nLivro adicionado com Sucesso!!')
             print(livros)
+            sleep(2)
        else:
-           print(f'\n Não foi possivel adicionar, pois o livro "{nome}" já está adicionado a lista de livros da biblioteca com o respectivo id: {id}!')
-       
+           print(f'\n Não foi possivel adicionar, pois o livro "{nome}" já está adicionado a lista de livros da biblioteca!')
+           sleep(2)
+           
+    def listar_disponiveis():
+        livros_disponiveis = pd.DataFrame(livros).T
+        print(livros_disponiveis)
+        sleep(4)
 
 class usuario:
     def adicionar_usuario(id, nome):
-        usuario = {'Id do Usuário: ': {id}, 'Nome do Usuário:' : {nome}}
-        if nome not in usuarios:
-            usuarios[nome] = usuario
-            print("\nUsuário Adicionado com Sucesso!")
-        else:
-            print(f"\nNão foi possível adicionar, pois já existe um usuário com esse ID!")
-        
+        usuario = {'Id do Usuário: ': id, 'Nome do Usuário:' : nome}
+        for dado in usuarios.values():
+            if id == dado['Id do Usuário: ']: #cap2 slide DataCamp
+                print(f"\nNão foi possível adicionar, pois já existe um usuário com o ID {id}, cujo nome é {dado['Nome do Usuário:']}!")
+                sleep(2)
+                return # O uso do return é para encerrar a função pois já existe o ID
+        usuarios[nome] = usuario
+        print("\nUsuário Adicionado com Sucesso!")
+        sleep(2)
+            
+    def listar_usuarios():
+        lista_usuarios = pd.DataFrame(usuarios)
+        print(lista_usuarios)
+        sleep(4)      
+            
 class emprestimo:
     def emprestar_livro(nomelivro, nomeusuario):
-        livro = {'Nome do Livro:': {nomelivro}, 'Nome do usuário responsável' : {nomeusuario}} 
+        livro = {'Nome do Livro:': nomelivro, 'Nome do usuário responsável' : nomeusuario} 
         if nomelivro in livros:
             if nomelivro not in livros_emprestados:
                 livros_emprestados[nomelivro] = livro
@@ -38,9 +54,16 @@ class emprestimo:
                 del(livros[nomelivro])    
             else:
                 print(f'\nO livro "{nomelivro}" já foi emprestado! Tente novamente.')
+            sleep(2)
         else:
             print("\nO livro não consta dentre os disponiveis para o empréstimo! Tente novamente.")
-                
+            sleep(2)
+            
+    def listar_emprestados():
+        emprestados = pd.DataFrame(livros_emprestados).T
+        print(emprestados) 
+        sleep(4)
+        
 class devolucao:
     def devolver_livro(nomelivro, nomeusuario, idlivro, nomeautor):
         if nomelivro in livros_emprestados:
@@ -50,10 +73,39 @@ class devolucao:
                 livros[nomelivro] = livro
                 print("Livro devolvido com sucesso!")
                 print(livros)
+                sleep(2)
         else:
             print(f"\nO livro {nomelivro} não consta na lista de livros emprestados ou não existe! Tente novamente.")
+            sleep(2) 
+
+# FUNÇÕES DE ARQUIVO:
+         
+def salvar_livros():
+    with open('livros.csv', 'w', newline="") as csvfile: 
+        cabecalio = ['Id do Livro', 'Nome do Livro', 'Nome do Autor']
+        writer = csv.DictWriter(csvfile, fieldnames = cabecalio) # Fieldnames é o mesmo que cabeçalio
+        for livro, dados_livros in livros.items(): # Livro: keys | Dados_livros: values
+            # Mapeamento do dict:
+            writer.writerow({
+                'Id do Livro' : dados_livros['Id do Livro:'],
+                'Nome do Livro' : dados_livros['Nome do Livro:'], # Lembrar das virgulas!
+                'Nome do Autor' : dados_livros['Autor do Livro:']
+                    })
             
-        
+def ler_livros():
+    with open('livros.csv', 'r', newline='') as csvfile:
+        if os.path.exists('livros.csv'): # Pois se ainda não existir, não há como fazer leitura e apresenta erro!
+            reader = csv.DictReader(csvfile)
+            for row in reader: # Extração das informações
+                idlivro = row['Id do Livro']
+                nomelivro = row['Nome do Livro']
+                autorlivro = row['Nome do Autor']
+
+                livros[nomelivro] = {'Id do Livro:' : idlivro,  'Nome do Livro:' : nomelivro, 'Autor do Livro:' : autorlivro}
+    
+
+# Programa Principal: 
+
 def principal():
     while True:
         print("\n----- Sistema de Gerenciamento da Biblioteca -----")
@@ -62,7 +114,12 @@ def principal():
         print("3. Emprestar Livro")
         print("4. Devolver Livro")
         print("5. Listar Livros Disponíveis")
-        print("6. Sair")
+        print("6. Listar Livros Emprestados")
+        print("7. Listar Usuários")
+        print("8. Sair")
+        
+        salvar_livros()
+        ler_livros()
         
         op = int(input("Digite a opção desejada: "))
     
@@ -89,10 +146,16 @@ def principal():
             nome_usuario = str(input("Digite o nome do usuário responsável: ")).upper().strip()
             devolucao.devolver_livro(nome_livro, nome_usuario, id_livro, autor_livro)
             
-        if op > 6:
-            print("\nOpção inválida, tente novamente!")
-            
-        if op == 6:
+        elif op == 5:
+            livro.listar_disponiveis()
+        
+        elif op == 6:
+            emprestimo.listar_emprestados()
+        
+        elif op == 7:
+            usuario.listar_usuarios()
+        
+        elif op == 8:
             print("Fechando sitema...")
             sleep(1)
             print('3')
@@ -102,5 +165,8 @@ def principal():
             print('1')
             break   
         
-        
+        else:
+            if op > 8:
+                print("\nOpção inválida, tente novamente!")
+                    
 principal()
